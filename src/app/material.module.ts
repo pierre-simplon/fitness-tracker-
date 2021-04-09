6469b34891e5408f76e46d9ce0d5bc0f27c955cf
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-04-09 10:34:13 +0200

    Add navigation & menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import { MatDatepickerModule} from '@angular/material/datepicker'
 import { MatNativeDateModule} from '@angular/material/core'
 import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule} from '@angular/material/toolbar'
 
 const modules = [
   MatButtonModule,
@@ -17,6 +18,7 @@ const modules = [
   MatInputModule,
   MatCheckboxModule,
   MatSidenavModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
