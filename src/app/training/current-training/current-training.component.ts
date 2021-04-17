9c343c7c60b3a0f11e053ecfa6754304fb36ab2f
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-04-17 10:26:40 +0200

    Fix timeOut type mismatch

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -24,7 +24,7 @@ export class CurrentTrainingComponent implements OnInit {
 
   startOrResumeTimer() {
     const step = this.trainingService.getRunningExercise().duration / 100 * 1000
-    this.timer = setInterval(()=> {
+    this.timer = window.setInterval(()=> {
       this.progress = this.progress + 5
       if (this.progress >=100) {
         clearInterval(this.timer)
