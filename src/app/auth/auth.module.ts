a21ac134ece0562430139594f2309aa8c5194e4e
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-05-05 08:42:43 +0200

    Split auth routing module from app routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { AngularFireModule } from "@angular/fire";
 import { ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
+import { AuthRoutingModule } from "./auth-routing.module";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 
@@ -13,6 +14,7 @@ import { SignupComponent } from "./signup/signup.component";
     ReactiveFormsModule,
     SharedModule,
     AngularFireModule,
+    AuthRoutingModule,
   ],
   exports: []
 })
