4327482f6330ea892de057700318dcb6190c3714
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-05-06 10:09:48 +0200

    Update prod env setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA92mcgAlAyhViUexZ-i18OhCkcVtpgHA4",
+    authDomain: "fitness-tracker-bcc28.firebaseapp.com",
+    projectId: "fitness-tracker-bcc28",
+    storageBucket: "fitness-tracker-bcc28.appspot.com",
+    messagingSenderId: "329650194168",
+    appId: "1:329650194168:web:fbf1c49ca965e7477e1d0d"
+  }
 };
