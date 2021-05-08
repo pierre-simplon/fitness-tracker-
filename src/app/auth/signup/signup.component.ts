299094c86c4ad2ecf7530eaee726b468eda9d69b
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-05-08 17:14:23 +0200

    Use store for signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,34 +1,31 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,Validators } from '@angular/forms';
-import { ReplaySubject, Subscription } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { UIService } from 'src/app/shared/ui.service';
 import { AuthService } from '../auth.service';
+import * as fromRoot from '../../app.reducer';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit,OnDestroy {
+export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
   maxDate: Date;
-  isLoading: boolean = false;
-  destroyed$ = new ReplaySubject(1);
-  private loadingSubscription:Subscription;
-
+  isLoading$: Observable<boolean>;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private uiservice: UIService,
+    private store: Store<fromRoot.State>,
     )
     { }
 
   ngOnInit(): void {
-    this.loadingSubscription = this.uiservice.loadingStateChanged
-    .pipe(takeUntil(this.destroyed$))
-    .subscribe(isLoading => {this.isLoading = isLoading});
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading)
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
     this.signUpForm = this.formBuilder.group({
@@ -51,8 +48,4 @@ export class SignupComponent implements OnInit,OnDestroy {
     this.uiservice.loadingStateChanged.next(true);
   }
 
-  ngOnDestroy(){
-    this.destroyed$.next(true);
-    this.destroyed$.complete();
-  }
 }
