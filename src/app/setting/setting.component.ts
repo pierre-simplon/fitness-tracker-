36e74ade804047ffbc4f751bb4ee71c3aa089c0c
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-05-23 17:41:05 +0200

    Initialize Edit exercice in Setting module

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { TrainingService } from '../training/training.service';
+import * as fromTraining from '../training/training.reducer'
+import * as fromRoot from '../app.reducer'
+import { NgForm } from '@angular/forms';
+import { Exercise } from '../training/exercise.model';
 
 @Component({
   selector: 'app-setting',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.component.css']
 })
 export class SettingComponent implements OnInit {
+  exercises$: Observable<Exercise[]>;
+  isLoading$: Observable<boolean>;
 
-  constructor() { }
+  constructor(
+    private trainingService: TrainingService,
+    private store: Store<fromTraining.State>,
+    ) {  }
 
-  ngOnInit(): void {
-  }
 
+    ngOnInit(): void {
+      this.isLoading$ = this.store.select(fromRoot.getIsLoading);
+      this.exercises$ = this.store.select(fromTraining.getAvailableExercises)
+    }
+
+    onEditTraining(form: NgForm) {
+      this.trainingService.EditTraining(form.value.exercise);
+    }
+
+    fetchExercises(){
+      this.exercises$ = this.store.select(fromTraining.getAvailableExercises)
+    }
 }
--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -1,6 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { StoreModule } from "@ngrx/store";
 import { SharedModule } from "../shared/shared.module";
+import { trainingReducer } from "../training/training.reducer";
 import { SettingComponent } from "./setting.component";
 import { SettingsRoutingModule } from "./settings-routing.module";
 
@@ -13,6 +15,7 @@ import { SettingsRoutingModule } from "./settings-routing.module";
     SharedModule,
     FlexLayoutModule,
     SettingsRoutingModule,
+    StoreModule.forFeature('training',trainingReducer),
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
