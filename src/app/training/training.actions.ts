ee8f844082854853ddc56da3bac9615ef3afe1e6
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-06-18 09:21:14 +0200

    Add Remove Training actions

--- src/app/training/training.actions.ts
+++ src/app/training/training.actions.ts
@@ -7,6 +7,8 @@ export const START_TRAINING = '[Training] Start Training';
 export const STOP_TRAINING = '[Training] Stop Training';
 export const START_EDIT_TRAINING = '[Training] Start Edit Training';
 export const STOP_EDIT_TRAINING = '[Training] Stop Edit Training';
+export const START_REMOVE_TRAINING = '[Training] Start Remove Training';
+export const STOP_REMOVE_TRAINING = '[Training] Stop Remove Training';
 
 export class SetAvailableTrainings implements Action {
   readonly type = SET_AVAILABLE_TRAININGS;
@@ -41,8 +43,20 @@ export class StopEditTraining implements Action {
   readonly type = STOP_EDIT_TRAINING;
 }
 
+export class StartRemoveTraining implements Action {
+  readonly type = START_REMOVE_TRAINING;
+  constructor(
+    public payload: string
+  ) { }
+}
+
+
+export class StopRemoveTraining implements Action {
+  readonly type = STOP_REMOVE_TRAINING;
+}
+
 export class StopTraining implements Action {
   readonly type = STOP_TRAINING
 }
 
-export type TrainingActions = SetAvailableTrainings | SetFinishedTrainings | StartTraining | StopTraining | StartEditTraining | StopEditTraining;
+export type TrainingActions = SetAvailableTrainings | SetFinishedTrainings | StartTraining | StopTraining | StartEditTraining | StopEditTraining | StartRemoveTraining | StopRemoveTraining;
