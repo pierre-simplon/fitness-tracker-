87aba5cbaeb7ce228b7845168213a4509984b1e2
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-08-01 10:35:36 +0200

    Fix lint issue

--- src/app/setting/dialog-training/dialog-training.component.ts
+++ src/app/setting/dialog-training/dialog-training.component.ts
@@ -1,16 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-training',
   templateUrl: './dialog-training.component.html',
-  styleUrls: ['./dialog-training.component.css']
+  styleUrls: ['./dialog-training.component.css'],
 })
-export class DialogTrainingComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) { }
-
-  ngOnInit(): void {
-  }
-
+export class DialogTrainingComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
 }
--- src/app/training/stop-training/stop-training.component.ts
+++ src/app/training/stop-training/stop-training.component.ts
@@ -1,18 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core'
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-
-
 
 @Component({
   selector: 'app-stop-training',
   templateUrl: './stop-training.component.html',
-  styleUrls: ['./stop-training.component.css']
+  styleUrls: ['./stop-training.component.css'],
 })
-export class StopTrainingComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) { }
-
-  ngOnInit(): void {
-  }
-
+export class StopTrainingComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
 }
